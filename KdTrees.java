8cdebb41359273465632070009b6eca806e9ba10
sjgv <salvador.gvz@gmail.com>
2017-04-07 13:53:33 -0600

    Add files via upload

--- KdTrees.java
+++ KdTrees.java
@@ -4,10 +4,10 @@ import edu.princeton.cs.algs4.RectHV;
 import edu.princeton.cs.algs4.StdDraw;
 
 public class KdTrees<Value> {
-    private static final double XMIN = 0.0;
-    private static final double XMAX = 1.0;
-    private static final double YMIN = 0.0;
-    private static final double YMAX = 1.0;
+    private static final double negXinf = 0.0;
+    private static final double posXinf = 1.0;
+    private static final double negYinf = 0.0;
+    private static final double posYinf = 1.0;
 
     private int size;
 
@@ -44,7 +44,7 @@ public class KdTrees<Value> {
     }
 
     public void put(Point2D p, Value val) {
-        root = put(root, p,val, XMIN, YMIN, XMAX, YMAX, 0);
+        root = put(root, p,val, negXinf, negYinf, posXinf, posYinf, 0);
     }
     
     public Value get(Point2D p){
@@ -55,23 +55,22 @@ public class KdTrees<Value> {
         return (contains(root, p, 0) != null);
     }
 
-    private int cmp(Point2D a, Point2D b, int level) {
+    private int cmp(Point2D fresh, Point2D old, int level) {
         if (level % 2 == 0) {
-            // Compare x-coordinates
-            int cmpResult = new Double(a.x()).compareTo(new Double(b.x()));
-            if (cmpResult == 0) {
-                return new Double(a.y()).compareTo(new Double(b.y()));
+            //Compare x-coordinates
+            int cmp = Double.compare(fresh.x(), old.x());
+            if (cmp == 0) {
+                return Double.compare(fresh.y(), old.y());
             } else {
-                return cmpResult;
+                return cmp;
             }
         } else {
-         // Compare y-coordinates
-            int cmpResult = new Double(a.y()).compareTo(new Double(b.y()));
-
-            if (cmpResult == 0) {
-                return new Double(a.x()).compareTo(new Double(b.x()));
+         //Compare y-coordinates
+        	int cmp = Double.compare(fresh.y(), old.y());
+            if (cmp == 0) {
+                return Double.compare(fresh.x(),old.y());
             } else {
-                return cmpResult;
+                return cmp;
             }
         }
     }
@@ -83,6 +82,7 @@ public class KdTrees<Value> {
         };
 
         int cmp = cmp(p, N.p, level);
+
         if (cmp < 0) {
             if (level % 2 == 0) {
                 N.left = put(N.left, p, v, xmin, ymin, N.p.x(), ymax, level + 1);
@@ -99,53 +99,102 @@ public class KdTrees<Value> {
 
         return N;
     }
-    private Value get(Node N, Point2D p, int level){
+    
+    @SuppressWarnings("unchecked")
+	private Value get(Node N, Point2D p, int level){
     	if (N == null) {
             return null;
         };
         int cmp = cmp(p, N.p, level);
 
         if (cmp < 0) {
+        	Node nodel = (Node) get(N.left, p, level + 1);
             if (level % 2 == 0) {
-                N.left = (Node) get(N.left, p, level + 1);
+                N.left = nodel;
             } else {
-                N.left = (Node) get(N.left, p, level + 1);
+                N.left = nodel;
             }
         } else if (cmp > 0) {
-            if (level % 2 == 0) {
-                N.right = (Node) get(N.right, p, level + 1);
+            Node noder = (Node) get(N.right, p, level + 1);
+			if (level % 2 == 0) {
+                N.right = noder;
             } else {
-                N.right = (Node) get(N.right, p, level + 1);
+                N.right = noder;
             }
         }
 
         return N.val;
     }
 
-    private Point2D contains(Node x, Point2D point, int level) {
-        while (x != null) {
-
-            int cmp = cmp(point, x.p, level);
+    private Point2D contains(Node N, Point2D p, int level) {
+        while (N != null) {
+        	//fresh,old, level
+            int cmp = cmp(p, N.p, level);
 
             if (cmp < 0) {
-                return contains(x.left, point, level + 1);
+                return contains(N.left, p, level + 1);
             } else if (cmp > 0) {
-                return contains(x.right, point, level + 1);
+                return contains(N.right, p, level + 1);
             } else {
-                return x.p;
+                return N.p;
             }
         }
 
         return null;
     }
 
-    public void draw() {
-        StdDraw.clear();
-
-       // drawLine(root, 0);
+    public Iterable<Point2D> range(RectHV rect) {
+        Queue<Point2D> q = new Queue<Point2D>();
+        rangeAdd(root, rect, q);
+        return q;
     }
 
-   
+    private void rangeAdd(Node N, RectHV rect, Queue<Point2D> q) {
+        if (N != null && rect.intersects(N.rect)) {
+            if (rect.contains(N.p)) {
+                q.enqueue(N.p);
+            }
+            rangeAdd(N.left, rect, q);
+            rangeAdd(N.right, rect, q);
+        }
+    }
+
+    public Point2D nearest(Point2D p) {
+        if (isEmpty()) {
+            return null;
+        } else {
+            Point2D result = null;
+            result = nearest(root, p, result);
+            return result;
+        }
+    }
+
+    private Point2D nearest(Node N, Point2D p, Point2D minP) {
+        if (N != null) {
+            if (minP == null) {
+                minP = N.p;
+            }
+
+            // If the current min point is closer to query than the current point
+            //it's either going to be greater than or equal to
+            if (minP.distanceSquaredTo(p) >= N.rect.distanceSquaredTo(p)) {
+                if (N.p.distanceSquaredTo(p) < minP.distanceSquaredTo(p)) {
+                    minP = N.p;
+                }
+
+                // Check in which order should we iterate
+                if (N.right != null && N.right.rect.contains(p)) {
+                    minP = nearest(N.right, p, minP);
+                    minP = nearest(N.left, p, minP);
+                } else {
+                    minP = nearest(N.left, p, minP);
+                    minP = nearest(N.right, p, minP);
+                }
+            }
+        }
+
+        return minP;
+    }
 }
 
 
